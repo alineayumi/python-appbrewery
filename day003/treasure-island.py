e89a5ec56f726bfb96177d62cb5d663d879ae2d5
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2020-11-19 01:17:53 -0300

    fix: lower to treasure island

--- day003/treasure-island.py
+++ day003/treasure-island.py
@@ -25,11 +25,11 @@ print("Your mission is to find the treasure.")
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
-left_right = input('left or right?')
+left_right = input('left or right?').lower()
 if left_right == 'left':
-  swim_wait = input('swim or wait?')
+  swim_wait = input('swim or wait?').lower()
   if swim_wait == 'wait':
-    door = input('which door?')
+    door = input('which door?').lower()
     if door == 'red':
       print('Burned by fire. Game over')
     elif door == 'yellow':
