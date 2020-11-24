335fee7c35b2d5056ae54ebc7eb300d6b2f3a636
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2020-11-23 21:14:52 -0300

    day008

---
+++ day008/day-8-start.py
@@ -0,0 +1,15 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+  print('hello')
+  print('hello')
+  print('hello')
+
+def greet_with_name(name):
+  print(f'hello {name}')
+
+greet()
+greet_with_name('aline')
